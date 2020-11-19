dd5e211ea2c7a822b8f1917a87acfc3510fc314b
rolandcsa <roland.csatlos8@protonmail.com>
2020-11-19 15:05:27 +0100

    Added DefineBasicInfo class

---
+++ week-01/day-4/src/DefineBasicInfo.java
@@ -0,0 +1,14 @@
+public class DefineBasicInfo {
+    public static void main(String[] args) {
+        String name = "Roland Csatlos";
+        int age = 25;
+        double height = 1.85;
+        boolean married = false;
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(married);
+
+    }
+}
