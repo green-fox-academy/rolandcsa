1939aa0e6fc9a78a89fa1856df396b8a56be8516
rolandcsa <roland.csatlos8@protonmail.com>
2020-11-19 17:19:27 +0100

    Added two decimal precision feature

--- week-01/day-4/src/MileToKmConverter.java
+++ week-01/day-4/src/MileToKmConverter.java
@@ -7,7 +7,7 @@ public class MileToKmConverter {
         Scanner input = new Scanner(System.in);
         double miles = input.nextDouble();
         double km = miles * 1.609344;
-        System.out.printf("That is " + km + " kilometres.");
+        System.out.println("That is " + km + " kilometres.");
 
     }
 
