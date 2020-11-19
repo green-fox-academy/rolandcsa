3ec2292177164277b643d0c87c5b3e8ef5c8a22b
rolandcsa <roland.csatlos8@protonmail.com>
2020-11-19 13:44:58 +0100

    Added HumptyDumpty class

---
+++ week-01/day-4/src/HumptyDumpty.java
@@ -0,0 +1,12 @@
+public class HumptyDumpty {
+    public static void main(String[] args) {
+        System.out.println("Humpty Dumpty");
+        System.out.println("Sat on a wall,");
+        System.out.println("Humpty Dumpty");
+        System.out.println("Had a great fall.");
+        System.out.println("All the king`s horses,");
+        System.out.println("And all the king`s men");
+        System.out.println("Couldn`t put Humpty");
+        System.out.println("Together again.");
+    }
+}
