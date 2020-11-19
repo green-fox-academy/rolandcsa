b1f6549a34c46c8b7fd7a7fbd5e03b4bdb8e7e02
rolandcsa <roland.csatlos8@protonmail.com>
2020-11-19 13:54:59 +0100

    Added IntroduceYourself class

---
+++ week-01/day-4/src/IntroduceYourself.java
@@ -0,0 +1,7 @@
+public class IntroduceYourself {
+    public static void main(String[] args) {
+        System.out.println("Roland Csatlos");
+        System.out.println("25");
+        System.out.println("1.85");
+    }
+}
