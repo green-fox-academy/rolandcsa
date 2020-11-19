dec6982e69d2f3d66221410ccfd68c3b254f2553
rolandcsa <roland.csatlos8@protonmail.com>
2020-11-19 13:49:12 +0100

    Added HelloOthers class

---
+++ week-01/day-4/src/HelloOthers.java
@@ -0,0 +1,7 @@
+public class HelloOthers {
+    public static void main(String[] args) {
+        System.out.println("Hello Marci");
+        System.out.println("Hello Andris");
+        System.out.println("Hello Peti");
+    }
+}
