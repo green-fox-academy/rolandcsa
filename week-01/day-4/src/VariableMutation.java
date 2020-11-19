b62e494cd501ce918bc6305510f854ad18bba4e1
rolandcsa <roland.csatlos8@protonmail.com>
2020-11-19 15:09:06 +0100

    Added VariableMutation class

---
+++ week-01/day-4/src/VariableMutation.java
@@ -0,0 +1,12 @@
+public class VariableMutation {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 100;
+
+        a = a + 10;
+        b = b - 7;
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
