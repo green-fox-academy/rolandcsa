979f413993030c6afa45a0c07e083c12d4bb2c87
rolandcsa <roland.csatlos8@protonmail.com>
2020-11-20 11:01:51 +0100

    Added IWontCheatOnTheExam class

---
+++ week-01/day-5/src/IWontCheatOnTheExam.java
@@ -0,0 +1,14 @@
+// Create a program that writes this line 100 times:
+// "I won't cheat on the exam!"
+
+public class IWontCheatOnTheExam {
+    public static void main(String[] args) {
+        int a = 0;
+
+        while (a <= 100) {
+            a++;
+            System.out.println("I won`t cheat on the exam!");
+        }
+
+    }
+}
