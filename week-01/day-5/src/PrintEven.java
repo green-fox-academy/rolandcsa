dbcd2c57f962a4bbff6df9e2e4f3f8f3f6bb7c5c
rolandcsa <roland.csatlos8@protonmail.com>
2020-11-20 11:15:28 +0100

    Added PrintEven class

---
+++ week-01/day-5/src/PrintEven.java
@@ -0,0 +1,14 @@
+// Create a program that prints all the even numbers between 0 and 500
+
+public class PrintEven {
+    public static void main(String[] args) {
+        for (int a = 0; a <= 500; a++) {
+            if (a % 2 == 0) {
+                System.out.println(a);
+
+            }
+        }
+
+
+    }
+}
