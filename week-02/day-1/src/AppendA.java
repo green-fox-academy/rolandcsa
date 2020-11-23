7b0ab40c92562c96ccd915a7266952d1a1cffc70
rolandcsa <roland.csatlos8@protonmail.com>
2020-11-23 17:36:41 +0100

    Added AppendA class

---
+++ week-02/day-1/src/AppendA.java
@@ -0,0 +1,13 @@
+// - Create an array variable named `animals`
+//   with the following content: `["koal", "pand", "zebr"]`
+// - Add all elements an `"a"` at the end
+
+public class AppendA {
+    public static void main(String[] args) {
+        String[] animals = {"koal", "pand", "zebr"};
+        animals = animals + "a";
+
+//    Not finished
+
+    }
+}
