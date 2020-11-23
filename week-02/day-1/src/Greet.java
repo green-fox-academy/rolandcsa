7b8389b3677dcca2429ec97caa8d9ac23d7da1c5
rolandcsa <roland.csatlos8@protonmail.com>
2020-11-23 14:40:58 +0100

    Added Greet class

---
+++ week-02/day-1/src/Greet.java
@@ -0,0 +1,21 @@
+//  From now on, create the usual class wrapper
+//  and main method on your own.
+
+// - Create a string variable named `al` and assign the value `Green Fox` to it
+// - Create a function called `greet` that greets it's input parameter
+//     - Greeting is printing e.g. `Greetings dear, Green Fox`
+// - Greet `al`
+
+import java.util.Scanner;
+
+//public class Greet {
+//
+////    public static greet() {
+//        String al = "Green Fox";
+////        System.out.println("What is your name?");
+//    }
+//
+////    public static void main(String[] args) {
+
+//    }
+//}
