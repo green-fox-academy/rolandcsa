a7a098e61587ac937a3f10142c861795825cb420
rolandcsa <roland.csatlos8@protonmail.com>
2020-11-24 16:26:41 +0100

    Finished Reverse class

--- week-02/day-2/src/Reverse.java
+++ week-02/day-2/src/Reverse.java
@@ -14,8 +14,8 @@ public class Reverse {
     public static void main(String[] args) {
         String toBeReversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI";
 
-        System.out.println(toBeReversed);
-
-//        Not finished
+        for (int i = toBeReversed.length() - 1; i >= 0; i-- ) {
+            System.out.print(toBeReversed.charAt(i));
+        }
     }
 }
