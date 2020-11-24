feaa9c26a95de3bd8e593a8f15d97cdd919a4fc9
rolandcsa <roland.csatlos8@protonmail.com>
2020-11-24 16:57:38 +0100

    Added reverse function

--- week-02/day-2/src/Reverse.java
+++ week-02/day-2/src/Reverse.java
@@ -14,6 +14,10 @@ public class Reverse {
     public static void main(String[] args) {
         String toBeReversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI";
 
+        reverse(toBeReversed);
+    }
+
+    private static void reverse(String toBeReversed) {
         for (int i = toBeReversed.length() - 1; i >= 0; i-- ) {
             System.out.print(toBeReversed.charAt(i));
         }
