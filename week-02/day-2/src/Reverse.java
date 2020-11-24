95b16993b30e0466d03fc2b19243a769608dd9f7
rolandcsa <roland.csatlos8@protonmail.com>
2020-11-24 14:28:59 +0100

    Added Reverse class

---
+++ week-02/day-2/src/Reverse.java
@@ -0,0 +1,21 @@
+//public class Reverse {
+//    public static void main(String... args){
+//        String toBeReversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI";
+//
+//        // Create a method that can reverse a String, which is passed as the parameter
+//        // Use it on this reversed string to check it!
+//        // Try to solve this using charAt() first, and optionally anything else after.
+//
+//        System.out.println(reverse(toBeReversed));
+//    }
+//}
+
+public class Reverse {
+    public static void main(String[] args) {
+        String toBeReversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI";
+
+        System.out.println(toBeReversed);
+
+//        Not finished
+    }
+}
