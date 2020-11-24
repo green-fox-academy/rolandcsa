4c9056222edac322f88b7da3404ba1ebff69c3ba
rolandcsa <roland.csatlos8@protonmail.com>
2020-11-24 08:46:32 +0100

    Finished AppendA class

--- week-02/day-1/src/AppendA.java
+++ week-02/day-1/src/AppendA.java
@@ -2,12 +2,14 @@
 //   with the following content: `["koal", "pand", "zebr"]`
 // - Add all elements an `"a"` at the end
 
+import java.util.Arrays;
+
 public class AppendA {
     public static void main(String[] args) {
         String[] animals = {"koal", "pand", "zebr"};
-        animals = animals + "a";
-
-//    Not finished
-
+        for (int i = 0; i < animals.length; i++) {
+            animals[i] = animals[i] + "a";
+        }
+        System.out.println(Arrays.toString(animals));
     }
 }
