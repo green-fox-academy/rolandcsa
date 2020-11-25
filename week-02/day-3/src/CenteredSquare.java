f3d6c3c27379797d027bbb5289ee5d601a259866
rolandcsa <roland.csatlos8@protonmail.com>
2020-11-25 13:39:42 +0100

    Added CenteredSquare class

---
+++ week-02/day-3/src/CenteredSquare.java
@@ -0,0 +1,40 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class CenteredSquare {
+
+    public static void mainDraw(Graphics graphics) {
+        // Draw a green 10x10 square to the canvas' center.
+
+        graphics.setColor(Color.BLUE);
+        graphics.fillRect(WIDTH / 2 - 10, HEIGHT / 2 - 10, 10, 10);
+
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 720;
+    static int HEIGHT = 720;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            this.setBackground(Color.WHITE);
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}
