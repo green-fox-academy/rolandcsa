59df5d67145dadc8e73632161ea561b27c8279ed
rolandcsa <roland.csatlos8@protonmail.com>
2020-11-26 22:49:26 +0100

    Modified commented and added codeonly versions

--- week-02/day-4/src/CheckerboardCodeOnly.java
+++ week-02/day-4/src/CheckerboardCodeOnly.java
@@ -4,22 +4,15 @@ import java.awt.*;
 
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
-public class Checkerboard {
+public class CheckerboardCodeOnly {
 
     public static void mainDraw(Graphics graphics) {
         // Fill the canvas with a checkerboard pattern.
 
-//        The ready function:
-
         Checkmate(graphics);
     }
 
-
-//    Here I make the function "Checkmate".
-
     public static void Checkmate(Graphics graphics) {
-
-//      Here I declare some useful variables.
 
         int xAxis = 0;
         int yAxis = 0;
@@ -28,50 +21,19 @@ public class Checkerboard {
         int boardSize = 8;
         boolean isWhite = true;
 
-//        Here I made a nested for loop.
-//        Variable "rows" defines what happens in every row.
-//        Variable "columns" defines what happens in every column.
-
         for (int rows = 0; rows < boardSize; rows++) {
             for (int columns = 0; columns < boardSize; columns++) {
 
-//                Here I increase the  zero-point of the rectangle by 30,
-//                every time variable "columns" is smaller than variable "boardSize"
-//                This will give me 8 rectangles closely after each other in the top of the screen
-
                 xAxis += 30;
-
-//              Here I simply ask whether the "isWhite" boolean is true or not.
-//              If true, I color the rectangles white.
-//              If false, I color the rectangles black.
-
                 if (isWhite) {
                     graphics.setColor(Color.WHITE);
                 } else {
                     graphics.setColor(Color.BLACK);
                 }
-
-//              Here I make the rectangle with the necessary arguments(?)
-//              "xAxis" and "yAxis" defines the position,
-//              "width" and "height" defines the size of the rectangle.
-
                 graphics.fillRect(xAxis, yAxis, width, height);
-
-//              Every time the second loop is about to end,
-//              I change the color to its opposite value.
-//              It returns the checkerboard pattern.
-//              white-black-white-black
-
                 isWhite = !isWhite;
 
             }
-//          After the "columns" loop finished I start to make alterations in the "rows" loop.
-
-//          I set the xAxis zero-point value to 0, in other words,
-//          to the top left corner.
-//          With the increase of the yAxis I start to make the next row.
-
-//          I also change the starting color of every single row, thanks to the for loop.
 
             isWhite = !isWhite;
             xAxis = 0;
@@ -104,3 +66,4 @@ public class Checkerboard {
         }
     }
 }
+
