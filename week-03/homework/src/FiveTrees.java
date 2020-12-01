14f55f098a2aea4ccd9ee6d71d862f6d35a62d44
rolandcsa <roland.csatlos8@protonmail.com>
2020-12-01 21:24:01 +0100

    Added FiveTrees class

---
+++ week-03/homework/src/FiveTrees.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+import java.util.ArrayList;
+
+public class FiveTrees {
+    public static void main(String[] args) {
+        ArrayList<String> types = new ArrayList<>();
+        ArrayList<Color> leafColor = new ArrayList<>();
+        ArrayList<Integer> age = new ArrayList<>();
+        ArrayList<String> sex = new ArrayList<>();
+    }
+}
