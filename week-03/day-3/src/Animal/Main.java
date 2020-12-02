e7b2934bd42caea983ee900193c93010fc0c09c9
rolandcsa <roland.csatlos8@protonmail.com>
2020-12-02 17:27:14 +0100

    Modified Main class

--- week-03/day-3/src/Animal/Main.java
+++ week-03/day-3/src/Animal/Main.java
@@ -2,6 +2,16 @@ package Animal;
 
 public class Main {
     public static void main(String[] args) {
+        Animal dog = new Animal();
+        System.out.println("Hunger: " + dog.hunger);
+        System.out.println("Thirst: " + dog.thirst);
 
+        dog.eat();
+        System.out.println("Hunger: " + dog.hunger);
+        dog.drink();
+        System.out.println("Thirst: " + dog.thirst);
+        dog.play();
+        System.out.println("Hunger: " + dog.hunger);
+        System.out.println("Thirst: " + dog.thirst);
     }
 }
