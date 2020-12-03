4a5eeba0d5db41cd1e5caa3651c3648767304c3a
rolandcsa <roland.csatlos8@protonmail.com>
2020-12-03 14:14:39 +0100

    Added instructions

--- week-03/day-4/src/sharpieSet/Main.java
+++ week-03/day-4/src/sharpieSet/Main.java
@@ -1,3 +1,11 @@
+//# Sharpie Set
+//
+//        - Reuse your `Sharpie` class
+//- Create `SharpieSet` class
+//  - it contains a list of Sharpie
+//          - countUsable() -> sharpie is usable if it has ink in it
+//          - removeTrash() -> removes all unusable sharpies
+
 package sharpieSet;
 
 public class Main {
