90257cd5af245828192ecd2b10d237111f80ff99
rolandcsa <roland.csatlos8@protonmail.com>
2020-12-03 10:42:53 +0100

    Added DiceSet package

---
+++ week-03/day-4/src/DiceSet/Main.java
@@ -0,0 +1,23 @@
+package DiceSet;
+
+public class Main {
+    public static void main(String[] args) {
+        // You have a `DiceSet` class which has a list for 6 dice
+        // You can roll all of them with roll()
+        // Check the current rolled numbers with getCurrent()
+        // You can reroll with reroll()
+        // Your task is to roll the dice until all of the dice are 6
+
+        DiceSet diceSet = new DiceSet();
+        System.out.println(diceSet.getCurrent());
+        System.out.println(diceSet.roll());
+        System.out.println(diceSet.getCurrent());
+        System.out.println(diceSet.getCurrent(5));
+        diceSet.reroll();
+        System.out.println(diceSet.getCurrent());
+        diceSet.reroll(4);
+        System.out.println(diceSet.getCurrent());
+        diceSet.rollSixes();
+        System.out.println(diceSet.getCurrent());
+    }
+}
