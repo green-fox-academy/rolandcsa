5e1dd28b256876172b48c7e922550e31b8986cfe
rolandcsa <roland.csatlos8@protonmail.com>
2020-12-03 15:53:18 +0100

    Modified petrolStation package

--- week-03/day-4/src/petrolStation/Main.java
+++ week-03/day-4/src/petrolStation/Main.java
@@ -1,3 +1,17 @@
+//# Petrol Station
+//
+//        - Create `Station` and `Car` classes
+//        - `Station`
+//        - gasAmount
+//        - refill(car) -> decreases the gasAmount by the capacity of the car and
+//        increases the cars gasAmount
+//        - `Car`
+//        - gasAmount
+//        - capacity
+//        - create constructor for `Car` where:
+//        - initialize gasAmount -> 0
+//        - initialize capacity -> 100
+
 package petrolStation;
 
 public class Main {
