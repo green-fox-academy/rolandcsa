153ed4ae49124d473925fa24e79f70338727f36e
rolandcsa <roland.csatlos8@protonmail.com>
2020-12-03 13:17:10 +0100

    Added teachersAndStudents package

---
+++ week-03/day-4/src/teachersAndStudents/Student.java
@@ -0,0 +1,10 @@
+package teachersAndStudents;
+
+public class Student {
+    public static void learn() {
+        System.out.println("The student is learning something new.");
+    }
+    public void question() {
+        Teacher.answer();
+    }
+}
---
+++ week-03/day-4/src/teachersAndStudents/Teacher.java
@@ -0,0 +1,10 @@
+package teachersAndStudents;
+
+public class Teacher {
+    public static void answer() {
+        System.out.println("The teacher is answering.");
+    }
+    public void teach() {
+        Student.learn();
+    }
+}
