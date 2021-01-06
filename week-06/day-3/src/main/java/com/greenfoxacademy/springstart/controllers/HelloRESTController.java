44915e241648307e950305d2bb39a33a5a38bd2f
rolandcsa <roland.csatlos8@protonmail.com>
2021-01-06 15:00:51 +0100

    Improved greeting method

--- week-06/day-3/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
+++ week-06/day-3/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
@@ -6,11 +6,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 @RestController
 public class HelloRESTController {
 
+    AtomicLong atomicLong = new AtomicLong();
+
     @RequestMapping(method = RequestMethod.GET, value = "/greeting")
     public Greeting greeting(@RequestParam String name) {
-        return new Greeting(1L, "Hello, " + name);
+        return new Greeting(atomicLong.getAndIncrement(), "Hello, " + name);
     }
 }
