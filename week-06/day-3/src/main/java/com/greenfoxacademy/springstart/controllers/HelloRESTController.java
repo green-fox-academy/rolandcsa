91ded1b12d35badab3a3301bee013d558d5928b6
rolandcsa <roland.csatlos8@protonmail.com>
2021-01-06 14:55:33 +0100

    Improved greeting function

--- week-06/day-3/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
+++ week-06/day-3/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
@@ -3,13 +3,14 @@ package com.greenfoxacademy.springstart.controllers;
 import com.greenfoxacademy.springstart.model.Greeting;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloRESTController {
 
     @RequestMapping(method = RequestMethod.GET, value = "/greeting")
-    public Greeting greeting() {
-        return new Greeting(1L, "Hello World!");
+    public Greeting greeting(@RequestParam String name) {
+        return new Greeting(1L, "Hello, " + name);
     }
 }
