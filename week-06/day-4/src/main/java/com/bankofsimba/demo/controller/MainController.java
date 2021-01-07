f098e4d87ade85d5a7c099bbd615e790ef405c78
rolandcsa <roland.csatlos8@protonmail.com>
2021-01-07 14:40:28 +0100

    Added accounts function

--- week-06/day-4/src/main/java/com/bankofsimba/demo/controller/MainController.java
+++ week-06/day-4/src/main/java/com/bankofsimba/demo/controller/MainController.java
@@ -5,20 +5,36 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class MainController {
     BankAccount simba = new BankAccount("Simba", 2000, "lion");
 
     @GetMapping("/show")
-    public String show(Model model){
+    public String show(Model model) {
         model.addAttribute("name", simba.getName());
         model.addAttribute("balance", simba.getBalance());
         model.addAttribute("animalType", simba.getAnimalType());
         return "show";
     }
+
     @GetMapping("/text")
-    public String text(Model model){
+    public String text(Model model) {
         model.addAttribute("text", "This is an <em>HTML</em> text. <b>Enjoy yourself!</b>");
         return "text";
+    }
+
+    @GetMapping("/accounts")
+    public String accounts(Model model) {
+        List<BankAccount> characters = new ArrayList<>();
+        characters.add(new BankAccount("Simba", 2000, "lion"));
+        characters.add(new BankAccount("Timon", 1500, "mongoose"));
+        characters.add(new BankAccount("Pumba", 1000, "mongoose"));
+        characters.add(new BankAccount("Nala", 3000, "lion"));
+        characters.add(new BankAccount("Mufasa", 1750, "lion"));
+        model.addAttribute("characters", characters);
+        return "accounts";
     }
 }
