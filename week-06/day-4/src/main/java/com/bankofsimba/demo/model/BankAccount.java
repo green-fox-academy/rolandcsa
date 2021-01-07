70275aa073492123aa8404609533eb08ad849234
rolandcsa <roland.csatlos8@protonmail.com>
2021-01-07 16:55:02 +0100

    Modified accounts function

--- week-06/day-4/src/main/java/com/bankofsimba/demo/model/BankAccount.java
+++ week-06/day-4/src/main/java/com/bankofsimba/demo/model/BankAccount.java
@@ -1,19 +1,16 @@
 package com.bankofsimba.demo.model;
 
-import java.util.concurrent.atomic.AtomicLong;
-
 public class BankAccount {
     private final String name;
     private final Double balance;
     private final String animalType;
-    private final Long id;
+    private final Boolean isKing;
 
-    public BankAccount(String name, Double balance, String animalType) {
-        AtomicLong counter = new AtomicLong();
+    public BankAccount(String name, Double balance, String animalType, Boolean isKing) {
         this.name = name;
         this.balance = balance;
         this.animalType = animalType;
-        this.id = counter.getAndIncrement();
+        this.isKing = isKing;
     }
 
     public String getName() {
@@ -28,11 +25,21 @@ public class BankAccount {
         return animalType;
     }
 
+    public Boolean getIsKing() {
+        return isKing;
+    }
+
     public String getCurrency() {
         return "Zebra";
     }
 
     public String getBalanceFormatted() {
         return String.format("%.2f", balance);
+    }
+    public String getColor(){
+        if (isKing){
+            return "red";
+        }
+        return "black";
     }
 }
