90e7da62ec694e54fe956ceb8785700aaae24b90
rolandcsa <roland.csatlos8@protonmail.com>
2021-01-18 15:53:20 +0100

    Modified list method

--- week-08/day-1/connectionwithmysql/src/main/java/com/greenfox/connectionwithmysql/controller/TodoController.java
+++ week-08/day-1/connectionwithmysql/src/main/java/com/greenfox/connectionwithmysql/controller/TodoController.java
@@ -1,10 +1,15 @@
 package com.greenfox.connectionwithmysql.controller;
 
+import com.greenfox.connectionwithmysql.model.Todo;
 import com.greenfox.connectionwithmysql.repository.TodoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 public class TodoController {
@@ -16,8 +21,9 @@ public class TodoController {
     }
 
     @GetMapping(value = {"/", "list"})
-    public String list(Model model) {
-        model.addAttribute("todos", todoRepository.findAll());
+    public String list(Model model, @RequestParam(required = false) Boolean isDone) {
+        List<Todo> todos = (List<Todo>) todoRepository.findAll();
+        model.addAttribute("todos", todos.stream().filter(x -> x.getDone()==isDone).collect(Collectors.toList()));
         return "todolist";
     }
 
