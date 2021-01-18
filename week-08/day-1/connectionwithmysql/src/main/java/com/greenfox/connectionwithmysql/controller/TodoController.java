6ab1b8545e2c4677f194c79456ba554e49f735c7
rolandcsa <roland.csatlos8@protonmail.com>
2021-01-18 16:57:25 +0100

    Added newtodo function

--- week-08/day-1/connectionwithmysql/src/main/java/com/greenfox/connectionwithmysql/controller/TodoController.java
+++ week-08/day-1/connectionwithmysql/src/main/java/com/greenfox/connectionwithmysql/controller/TodoController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
@@ -23,12 +24,27 @@ public class TodoController {
     @GetMapping(value = {"/", "list"})
     public String list(Model model, @RequestParam(required = false) Boolean isDone) {
         List<Todo> todos = (List<Todo>) todoRepository.findAll();
-        model.addAttribute("todos", todos.stream().filter(x -> x.getDone()==isDone).collect(Collectors.toList()));
+        if (isDone == null) {
+            model.addAttribute("todos", todoRepository.findAll());
+        } else {
+            model.addAttribute("todos", todos.stream().filter(x -> x.getDone() == !isDone).collect(Collectors.toList()));
+        }
         return "todolist";
     }
 
     @GetMapping("todo")
     public String todo() {
+        return "redirect:/";
+    }
+
+    @GetMapping("newtodo")
+    public String getNewTodo() {
+        return "newtodo";
+    }
+
+    @PostMapping("newtodo")
+    public String postNewTodo(@RequestParam String title) {
+        todoRepository.save(new Todo(title));
         return "redirect:/";
     }
 }
