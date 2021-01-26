8749f12b8573b5e52336931988ce935ad7f0fedf
rolandcsa <roland.csatlos8@protonmail.com>
2021-01-26 09:30:00 +0100

    Modified greeter function

--- week-09/day-1/src/main/java/com/greenfox/api/model/greeter/Greeter.java
+++ week-09/day-1/src/main/java/com/greenfox/api/model/greeter/Greeter.java
@@ -1,5 +1,6 @@
 package com.greenfox.api.model.greeter;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -8,9 +9,10 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 public class Greeter {
-    private String welcome_message;
+    @JsonProperty(value = "welcome_message")
+    private String welcomeMessage;
 
     public Greeter(String name, String title) {
-        this.welcome_message = "Oh, hi there " + name + ", my dear " + title + "!";
+        this.welcomeMessage = "Oh, hi there " + name + ", my dear " + title + "!";
     }
 }
